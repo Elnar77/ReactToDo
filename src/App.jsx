70c68774e21650ffa4e5ffe80253675582c22c91
Elnar S. <elnar.salimov@web.de>
2025-01-01 22:29:48 +0100

    Create blank Application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import CssBaseline from "@mui/material/CssBaseline"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+    </>
+  )
+}
+
+export default App
